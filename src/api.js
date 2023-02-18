ab3b696e77343241a1364cfaa205bc0d58a806e5
AndriySafonov <andreygold18@gmail.com>
2023-02-18 20:21:28 +0200

    markup

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const ENDPOINT = 'https://pixabay.com/api/';
+
+// const options = {
+//   key: '33719885-4078ecd8a7ef8c07d3287ea16',
+//   q: 'yellow+flowers',
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: 'true',
+// };
+
+function fetchData(query) {
+  return fetch(
+    `${ENDPOINT}?key=33719885-4078ecd8a7ef8c07d3287ea16&image_type=photo&orientation=horizontal&safesearch=true&q=${query}`
+  )
+    .then(response => response.json())
+    .then(({ hits }) => console.log(hits));
+}
+
+export default fetchData;
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import fetchData from './api.js';
+import Notiflix from 'notiflix';
+
+const form = document.getElementById('search-form');
+// const searchBtn = document.querySelector('.cearch-btn');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const value = form.elements.searchQuery.value.trim();
+
+  fetchData(value)
+    .then(({ hits }) => {
+      if (hits.length === 0)
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+
+      return hits.reduse((markup, hit) => createMarkup(hit) + markup, '');
+    })
+    .then(updateNewsList)
+    .catch(onError)
+    .finally(() => form.reset());
+}
+
+function updateNewsList(markup) {
+  gallery.insertAdjacentHTML('beforeend', markup)
+}
+
+function createMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads${downloads}</b>
+    </p>
+  </div>
+</div>`;
+}
+
+function onError(err) {
+  console.error(err);
+}
