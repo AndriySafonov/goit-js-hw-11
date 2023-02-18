b27f6aa801eeada308be99803e088089ac3cda4e
AndriySafonov <andreygold18@gmail.com>
2023-02-18 21:14:08 +0200

    update

--- src/api.js
+++ src/api.js
@@ -13,7 +13,7 @@ function fetchData(query) {
     `${ENDPOINT}?key=33719885-4078ecd8a7ef8c07d3287ea16&image_type=photo&orientation=horizontal&safesearch=true&q=${query}`
   )
     .then(response => response.json())
-    .then(({ hits }) => console.log(hits));
+    // .then(({ hits }) => console.log(hits));
 }
 
 export default fetchData;
