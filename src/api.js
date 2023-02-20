6a4377adaf98d6e36edf5fc934c45fcf7cd86bb4
AndriySafonov <andreygold18@gmail.com>
2023-02-20 12:38:22 +0200

    wrote differently

--- src/api.js
+++ src/api.js
@@ -1,24 +1,37 @@
 const ENDPOINT = 'https://pixabay.com/api/';
 const key = '33719885-4078ecd8a7ef8c07d3287ea16';
+const URL =
+  '?key=33719885-4078ecd8a7ef8c07d3287ea16&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=1';
 
-// const options = {
-//   key: '33719885-4078ecd8a7ef8c07d3287ea16',
-//   q: 'yellow+flowers',
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: 'true',
-// };
-
-function fetchData(q) {
+function searchFoto(page = 1) {
   return fetch(
-    `${ENDPOINT}?key=${key}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=1`
+    `${ENDPOINT}?key=${key}&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${page}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(respose.statusText);
     }
     return response.json();
   });
-  // .then(({ hits }) => console.log(hits));
 }
+export default searchFoto;
+// // const options = {
+// //   key: '33719885-4078ecd8a7ef8c07d3287ea16',
+// //   q: 'yellow+flowers',
+// //   image_type: 'photo',
+// //   orientation: 'horizontal',
+// //   safesearch: 'true',
+// // };
 
-export default fetchData;
+// function fetchData(q) {
+//   return fetch(
+//     `${ENDPOINT}?key=${key}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=1`
+// ).then(response => {
+//   if (!response.ok) {
+//     throw new Error(respose.statusText);
+//   }
+//   return response.json();
+// });
+//   // .then(({ hits }) => console.log(hits));
+// }
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import fetchData from './api.js';
+import searchFoto from './api.js';
 import Notiflix from 'notiflix';
 
 const form = document.getElementById('search-form');
@@ -8,75 +8,138 @@ const loadMoreBtn = document.querySelector('.load-more');
 form.addEventListener('submit', onSubmit);
 loadMoreBtn.addEventListener('click', loadMore);
 
-let page = 1;
-function loadMore() {
-  page += 1;
-  fetchData(q).then(data =>
-    createMarkup({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    })
-  );
-}
-
 function onSubmit(e) {
   e.preventDefault();
 
   const form = e.currentTarget;
   const value = form.elements.searchQuery.value.trim();
-
-  fetchData(value)
-    .then(({ hits }) => {
-      if (hits.length === 0)
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-
-      return hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-      loadMoreBtn.hidden = false;
-    })
-    .then(updateNewsList)
-    .catch(onError)
-    .finally(() => form.reset());
+  searchFoto(value).then(({ hits }) => {
+    if (hits.length === 0)
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+  });
 }
 
-function updateNewsList(markup) {
-  gallery.insertAdjacentHTML('beforeend', markup);
+let page = 1;
+function loadMore() {
+  page += 1;
+  searchFoto(page).then(data => {
+    renderMarcup(data.hits);
+  });
 }
 
-function createMarkup({
-  webformatURL,
-  largeImageURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
-  return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads${downloads}</b>
-    </p>
-  </div>
-</div>`;
+searchFoto()
+  .then(data => {
+    renderMarcup(data.hits);
+    loadMoreBtn.hidden = false;
+  })
+  .catch(error => console.log(error));
+
+function renderMarcup(arr) {
+  let marcup = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads${downloads}</b>
+        </p>
+      </div>
+    </div>`;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', marcup);
 }
 
-function onError(err) {
-  console.error(err);
-}
+// let page = 1;
+// function loadMore() {
+//   page += 1;
+//   fetchData(value).then(({ hits }) =>
+//     createMarkup({
+//       webformatURL,
+//       largeImageURL,
+//       tags,
+//       likes,
+//       views,
+//       comments,
+//       downloads,
+//     })
+//   );
+// }
+
+// function onSubmit(e) {
+//   e.preventDefault();
+
+//   const form = e.currentTarget;
+//   const value = form.elements.searchQuery.value.trim();
+
+//   fetchData(value)
+//     .then(({ hits }) => {
+//       if (hits.length === 0)
+//         Notiflix.Notify.failure(
+//           'Sorry, there are no images matching your search query. Please try again.'
+//         );
+
+//       return hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
+//       loadMoreBtn.hidden = false;
+//     })
+//     .then(updateNewsList)
+//     .catch(onError)
+//     .finally(() => form.reset());
+// }
+
+// function updateNewsList(markup) {
+//   gallery.insertAdjacentHTML('beforeend', markup);
+// }
+
+// function createMarkup({
+//   webformatURL,
+//   largeImageURL,
+//   tags,
+//   likes,
+//   views,
+//   comments,
+//   downloads,
+// }) {
+//   return `<div class="photo-card">
+//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes${likes}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views${views}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments${comments}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads${downloads}</b>
+//     </p>
+//   </div>
+// </div>`;
+// }
+
+// function onError(err) {
+//   console.error(err);
+// }
