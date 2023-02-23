bd7cd939eb65188018a3ad6e15885be6f73691b3
AndriySafonov <andreygold18@gmail.com>
2023-02-23 18:47:33 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,97 +1,171 @@
-import PhotoApiService from './PhotoApiService.js';
-import LoadMoreBtn from './components/LoadMoreBtn.js';
-import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+import { fetchSearchImage } from "./fetchSearchImage";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// SimpleLightbox
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const form = document.getElementById('search-form');
-const gallery = document.querySelector('.gallery');
 
-const photoApiService = new PhotoApiService();
-const loadMoreBtn = new LoadMoreBtn({
-  selector: '#loadMore',
-  isHidden: true,
-});
-console.log(photoApiService);
-console.log(loadMoreBtn);
+const formSearch = document.querySelector('#search-form');
+const galleryList = document.querySelector('.gallery');
+const btnLoad = document.querySelector('.load-more');
 
-loadMoreBtn.button.addEventListener('click', fetchHits);
-form.addEventListener('submit', onSubmit);
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
 
-function onSubmit(e) {
-  e.preventDefault();
+let page = 1;
+let searchQuery = '';
+let perPage = 40;
 
-  const form = e.currentTarget;
-  const value = form.elements.searchQuery.value.trim();
+formSearch.addEventListener('submit', onSearch);
+btnLoad.addEventListener('click', onButtonLoadMore);
 
-  photoApiService.searchQuery = value;
-  photoApiService.resetPage();
+btnLoad.style.display = "none";
 
-  clearPhotosList();
-  loadMoreBtn.show();
-  fetchHits().finally(() => form.reset());
-}
+// Пошук в search
 
-function fetchHits() {
-  loadMoreBtn.disable();
+async function onSearch(event){ 
+    event.preventDefault();
 
-  return photoApiService
-    .getPhotos()
-    .then(hits => {
-      if (hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
+    searchQuery = event.currentTarget.searchQuery.value.trim();
+    page = 1;
+
+    if(!searchQuery) {
+        galleryList.innerHTML = '';
+        return;
+    }
+
+    try {
+
+      const serchResponse = await fetchSearchImage(page, searchQuery);
+
+      Notify.info(`Hooray! We found ${serchResponse.totalHits} images.`);
+
+      if(serchResponse.totalHits === 0){
+        Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+        btnLoad.style.display = "none";
+      }else{
+        btnLoad.style.display = "block";
       }
 
-      return hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-      // loadMoreBtn.hidden = false;
+      createCardImg(serchResponse.hits)
+
+      //  без try..catch
+
+      // .then(imgSearchFeatch => {
+      //   Notify.info(`Hooray! We found ${imgSearchFeatch.totalHits} images.`);
+      //     if(imgSearchFeatch.totalHits === 0){
+      //       Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+      //       btnLoad.style.display = "none";
+      //     }else{
+      //       btnLoad.style.display = "block";
+      //     }
+      //     createCardImg(imgSearchFeatch.hits);
+      // })
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    lightbox.refresh();
+   
+};
+
+// при кліку загрузка ще контенту
+
+async function onButtonLoadMore() {  
+  page += 1;
+
+  try {
+    await fetchSearchImage(page, searchQuery).then(imgSearchFeatchMore => {
+
+        let totalPages = imgSearchFeatchMore.totalHits / perPage;
+        if (page >= totalPages) {
+            Notify.failure("We're sorry, but you've reached the end of search results");
+            btnLoad.style.display = "none";
+        }
+
+      createCardImg(imgSearchFeatchMore.hits);
+
+      const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+      });
+
     })
-    .then(markup => {
-      appendPhotosToList(markup);
-      loadMoreBtn.enable();
-    })
-    .catch(onError);
+      } catch (error) {
+        console.log(error); 
+      }
+    
+      lightbox.refresh();
 }
+    
 
-function appendPhotosToList(markup) {
-  gallery.insertAdjacentHTML('beforeend', markup);
-}
+  // try {
+  //   const btnResponse = await fetchSearchImage(page, searchQuery);
 
-function clearPhotosList() {
-  gallery.innerHTML = '';
-}
+  //   let totalPages = btnResponse.totalHits / perPage;
 
-function createMarkup({
-  webformatURL,
-  largeImageURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
-  return `
-  <a class="gallery__item" href="${largeImageURL}" onclick="return false;">
-  <div class="photo-card">
-  <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads${downloads}</b>
-    </p>
-  </div>
-</div>`;
-}
+  //   if (page >= totalPages) {
+  //       Notify.failure("We're sorry, but you've reached the end of search results");
+  //       btnLoad.style.display = "none";
+  //   }
+    
+  //   createCardImg(btnResponse.hits);
 
-function onError(err) {
-  console.error(err);
-  loadMoreBtn.hide();
-}
+  //   const { height: cardHeight } = galleryList.firstElementChild.getBoundingClientRect();
+
+  //   window.scrollBy({
+  //     top: cardHeight * 2,
+  //     behavior: "smooth",
+  //   });
+
+    // без try..catch
+    // .then(imgSearchFeatchMore => {
+    //   createCardImg(imgSearchFeatchMore.hits);
+    //   let totalPages = imgSearchFeatchMore.totalHits / perPage;
+    //   if (page >= totalPages) {
+    //       Notify.failure("We're sorry, but you've reached the end of search results");
+    //       btnLoad.style.display = "none";
+    //   }
+  
+    // })
+    
+ 
+
+// функція скрол
+// function onScroll() {
+  
+// }
+
+
+
+// функція яка робить вертку картинки 
+export function createCardImg(imgArr) {
+  galleryList.innerHTML = imgArr.map(img => 
+  `<div class="photo-card">
+      <div class="info">
+      <a href="${img.largeImageURL}" alt="${img.tags}" >
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" class="photo-img" />
+      </a>
+      <div class="info-flex">
+        <p class="info-item">
+          <b>Likes: ${img.likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${img.views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${img.comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${img.downloads}</b>
+        </p>
+      </div>
+      </div>
+  </div>`
+  ).join('')
+
+
+  
+};
+
